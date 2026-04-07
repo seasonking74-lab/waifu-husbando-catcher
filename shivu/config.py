677fbcd8e708205b608e9fea910782e40ba980af
seasonking74-lab <seasonking74@gmail.com>
2026-04-07 18:29:46 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,12 +1,8 @@
-import os
-
 class Config:
-    TOKEN = "8568309223:AAHWam_aBpk1K6pD29bf75nzwbEyoSFqxBU"
+    TOKEN = "8568309223:AAHWam_aBpk1K6pD29bf75nzwbEyoSFqxBU" 
     MONGO_URL = "mongodb+srv://season74_db_user:pass%401220@cluster0.nuxjsu4.mongodb.net/?retryWrites=true&w=majority"
     OWNER_ID = 8629880263
-    SUDO_USERS = [8629880263]
-    API_ID = 34967775
-    API_HASH = "e6e5dfae5327f90410863f93d8ced26b"
+    API_ID = 34708129
+    API_HASH = "21b314cceaa31514f684a695281ff3e2"
     SUPPORT_CHAT = "seasonartist12"
     UPDATE_CHAT = "grapsupdate"
-    PHOTO_URL = ["https://i.pinimg.com/736x/e6/d8/fc/e6d8fc8ebd8a6dde2e8ebba48def3843.jpg"]
