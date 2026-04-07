35d81ec3c8006650a1d02c488f4a1bb22e085f21
seasonking74-lab <seasonking74@gmail.com>
2026-04-07 18:02:47 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,24 +1,12 @@
-class Config(object):
-    LOGGER = True
+import os
 
-    # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
-
-    
-class Production(Config):
-    LOGGER = True
-
-
-class Development(Config):
-    LOGGER = True
+class Config:
+    TOKEN = "8568309223:AAHWam_aBpk1K6pD29bf75nzwbEyoSFqxBU"
+    MONGO_URL = "mongodb+srv://season74_db_user:pass%401220@cluster0.nuxjsu4.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = 8629880263
+    SUDO_USERS = [8629880263]
+    API_ID = 34967775
+    API_HASH = "e6e5dfae5327f90410863f93d8ced26b"
+    SUPPORT_CHAT = "seasonartist12"
+    UPDATE_CHAT = "grapsupdate"
+    PHOTO_URL = ["https://i.pinimg.com/736x/e6/d8/fc/e6d8fc8ebd8a6dde2e8ebba48def3843.jpg"]
