7f0334c6365a09df3bf966a7b65499f05bbdbab3
seasonking74-lab <seasonking74@gmail.com>
2026-04-07 17:12:53 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -1,43 +1,20 @@
-import logging  
-import os
-from pyrogram import Client 
-from telegram.ext import Application
+import logging
+from telegram.ext import ApplicationBuilder
 from motor.motor_asyncio import AsyncIOMotorClient
 
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
-)
+# Logs
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger('httpx').setLevel(logging.WARNING)
-logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
-LOGGER = logging.getLogger(__name__)
+# --- CONFIG ---
+TOKEN = "8568309223:AAHWam_aBpk1K6pD29bf75nzwbEyoSFqxBU"
+MONGO_URL = "mongodb+srv://season74_db_user:pass%401220@cluster0.nuxjsu4.mongodb.net/?retryWrites=true&w=majority"
+OWNER_ID = 8629880263
 
-from shivu.config import Development as Config
+# DB Connection
+client = AsyncIOMotorClient(MONGO_URL)
+db = client.grasp_pro
+chars_col = db.characters
+users_col = db.users
 
-
-api_id = Config.api_id
-api_hash = Config.api_hash
-TOKEN = Config.TOKEN
-GROUP_ID = Config.GROUP_ID
-CHARA_CHANNEL_ID = Config.CHARA_CHANNEL_ID 
-mongo_url = Config.mongo_url 
-PHOTO_URL = Config.PHOTO_URL 
-SUPPORT_CHAT = Config.SUPPORT_CHAT 
-UPDATE_CHAT = Config.UPDATE_CHAT
-BOT_USERNAME = Config.BOT_USERNAME 
-sudo_users = Config.sudo_users
-OWNER_ID = Config.OWNER_ID 
-
-application = Application.builder().token(TOKEN).build()
-shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
-lol = AsyncIOMotorClient(mongo_url)
-db = lol['Character_catcher']
-collection = db['anime_characters_lol']
-user_totals_collection = db['user_totals_lmaoooo']
-user_collection = db["user_collection_lmaoooo"]
-group_user_totals_collection = db['group_user_totalsssssss']
-top_global_groups_collection = db['top_global_groups']
-pm_users = db['total_pm_users']
+# Application
+application = ApplicationBuilder().token(TOKEN).build()
